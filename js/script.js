6c2f02b484dabb0b55d4ae530520a9688dd01367
Mohammed <mohd.ntw@gmail.com>
2021-09-03 17:15:19 -0600

    first complete draft

--- js/script.js
+++ js/script.js
@@ -10,14 +10,15 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+// Array of objects 
 const quotes = [
-  {"quote":"My quote","source":"Me","citation":"myDesk","year":2021},
+  {"quote":"My quote","source":"Me","citation":"myDesk","year":2021,"tag":"myTag"},
   {"quote":"We become what we think about","source":" Earl Nightingale"},
   {"quote":"Success is not final, failure is not fatal: it is the courage to continue that counts.","source":"Winston Churchill",},
   {"quote":"Happiness is not something readymade. It comes from your own actions.","source":"Dalai Lama","citation":"myDesk1","year":2021},
   {"quote":"Life is what happens when you’re busy making other plans.","source":"John Lennon",},
   {"quote":"My Money and success don’t change people; they merely amplify what is already there.","source":" Will Smith","year":2018},
-  {"quote":"I’m sick of following my dreams, man. I’m just going to ask where they’re going and hook up with ’em later.","source":"Mitch Hedberg","citation":"myDesk","year":2021},
+  {"quote":"I’m sick of following my dreams, man. I’m just going to ask where they’re going and hook up with ’em later.","source":"Mitch Hedberg","citation":"myDesk","year":2021,"tag":"humor"},
   {"quote":"Be the change that you wish to see in the world.","source":"Mahatma Gandhi","year":1989},
   {"quote":"If you tell the truth, you don't have to remember anything.","source":"Mark Twain","year":1935},
   {"quote":"Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.","source":"Martin Luther King Jr","year":1955 }
@@ -25,29 +26,49 @@ const quotes = [
 ]
 
 
+
+
+/*this variable is holding the random number used in the previos 
+function run forgetRandomQuote()  */
+let currentQuote = 0  ;
+
 /***
  * `getRandomQuote` function
+ * I added some logic in this function to make sure we don't end up using th same 
+ * randome number from last time so we get a new Quote guaranteed 
 ***/
 
 function getRandomQuote() {
-  let randomNumber =Math.floor(Math.random() * quotes.length);
+
+   let randomNumber = Math.floor(Math.random() * quotes.length);
+    
+  if (currentQuote === randomNumber) {
+    alert("match")
+    randomNumber = Math.floor(Math.random() * quotes.length);
+    //here we set the random nuber to currentQoute so next time we run the  getRandomQuote function we refrencit in the if statment
+    currentQuote = randomNumber
+    return  quotes[randomNumber];
+    
+  }
+  else {
    return quotes[randomNumber];
+  }
 }
-
-
-
-
 
 
 
 /***
  * `printQuote` function
+ * 
+ * in the print function we do couple of things , print the Qoute , call the function to genrate random RGB values
+ * clear and reset the refresh interval 
 ***/
 function printQuote  () {
-
+  // we save the returned value from the getRandom Quote into random Quote
   const randomQuote = getRandomQuote()
-  console.log(randomQuote)
+  //console.log(randomQuote)
 
+  // generating the html string to display into the webpage
   let htmString =
   `
   <p class="quote"> ${randomQuote.quote} </p>
@@ -62,23 +83,34 @@ function printQuote  () {
     htmString += `<span class="citation"> ${randomQuote.year}</span>`
   }
 
+  if (randomQuote["tag"]) {
+    htmString += `<span class="citation"> ${randomQuote.tag}</span>`
+  }
+
     htmString += `</p>`
 
-
+    // in this step will display the generated htm string 
     document.getElementById('quote-box').innerHTML = htmString;
-    //document.body.style.backgroundColor=`rgb(${randomColor ()},${randomColor ()},${randomColor ()})`
     
+    // this step we get the 3 random numbers for RGB in an array calling the randomColor()
     const rgbColors = randomColor()
+    // here we set  the rgb colo accssing the array values 
     document.body.style.backgroundColor=`rgb(${rgbColors[0]},${rgbColors[1]},${rgbColors[2]})`
     console.log(rgbColors)
 
-  
+   //in this step we clear the refresh time of the user click the button and reset the 10 seconds interval
+  clearInterval(myTimer)
+  myTimer = setInterval(function() {
+    printQuote()
+  }, 10000);
 
+  
 }
 
-// function randomColor () {
-//     return Math.floor(Math.random() * 255)
-// }
+/*** 
+ * the randomColor functino will genrate 3 random numbers between 0 and 255 and return it in an array
+
+***/
 
 function randomColor () {
   let rgb = []
@@ -90,10 +122,11 @@ function randomColor () {
   return rgb
 }
 
+//setting the refresh interval and calling the printQuote function
 
-
-
-
+let myTimer = setInterval(function() {
+  printQuote()
+}, 10000);
 
 
 
@@ -105,3 +138,6 @@ function randomColor () {
 
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
+
+
